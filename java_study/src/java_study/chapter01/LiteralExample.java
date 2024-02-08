a3e3e45dca82c7b64fbae556ce7860ce0af8b92f
Lee Joon Woo <wnsdn220@gmail.com>
2024-02-08 12:10:15 +0900

    LiteralExample

--- java_study/src/java_study/chapter01/LiteralExample.java
+++ java_study/src/java_study/chapter01/LiteralExample.java
@@ -1,5 +1,14 @@
 package java_study.chapter01;
 
 public class LiteralExample {
-	
+	public static void main(String[] args) {
+		int var1 = 10;
+		System.out.println(var1);
+		
+		int var2 = 010;
+		System.out.println(var2);
+		
+		int var3 = 0x10;
+		System.out.println(var3);
+	}
 }
